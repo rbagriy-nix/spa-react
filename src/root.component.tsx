1c1fe4217d801e45aa7f0e89fec879c5b3456153
rbagriy-nix <rbagriy@n-ix.com>
2021-02-25 10:02:39 +0200

    renamed component nav to home

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Route, BrowserRouter as Router } from "react-router-dom";
-import Nav from "./nav";
+import Home from "./Home";
 
 export default function Root() {
   return (
     <Router>
       <Route path="/react" exact>
-        <Nav />
+        <Home />
       </Route>
     </Router>
   );
