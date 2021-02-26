67fa2bcabe723b6aaeb57f1039731adda6f65125
rbagriy-nix <rbagriy@n-ix.com>
2021-02-26 12:31:12 +0200

    changes template

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,19 @@
 import React from "react";
 
 export default function Header() {
-    return (
-        <nav className="rNav">
-            <div className="logo">Fluke-Shop.</div>
-            <ul>
-                <li>Home</li>
-                <li>Our Products</li>
-                <li>About Us</li>
-                <li>Contact</li>
-
-            </ul>
-            <div className="search">
-                <i className="fa fa-search"></i>
-                <i className="fa fa-shopping-basket ml-2"></i>
-            </div>
-        </nav>
-    )
+  return (
+    <nav className="rNav">
+      <div className="logo">Shop.</div>
+      <ul>
+        <li>Home</li>
+        <li>Our Products</li>
+        <li>About Us</li>
+        <li>Contact</li>
+      </ul>
+      <div className="search">
+        <i className="fa fa-search"></i>
+        <i className="fa fa-shopping-basket ml-2"></i>
+      </div>
+    </nav>
+  );
 }
